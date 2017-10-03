ed579e499c401bdd6b692af36e00c890b9d31001
Pim Merks <pimmerks@msn.com>
2017-10-03 23:40:35 +0200

    Added MvvMLight

--- WebdavSync/View/MainWindow.xaml.cs
+++ WebdavSync/View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WebdavSync
+namespace WebdavSync.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
